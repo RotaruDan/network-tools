561131390de75982b766bebb502a4262d88fdf1d
anserran <angelserranolaguna@gmail.com>
2014-03-13 17:39:04 +0100

    More data printed in gui

--- network-gui/src/main/java/es/eucm/network/gui/NetworkGUI.java
+++ network-gui/src/main/java/es/eucm/network/gui/NetworkGUI.java
@@ -1,27 +1,32 @@
 package es.eucm.network.gui;
 
-import com.btr.proxy.search.ProxySearch;
 import es.eucm.network.JavaRequestHelper;
 import es.eucm.network.requests.RequestHelper;
 import es.eucm.network.requests.ResourceCallback;
 
-import java.net.ProxySelector;
+import java.util.Map.Entry;
 
 public class NetworkGUI {
 
 	public static void main(String args[]) {
-
 
 		String urlTest = "http://downloads.sourceforge.net/project/e-adventure/ead2-stable/update.json";
 		RequestHelper requestHelper = new JavaRequestHelper();
 		requestHelper.url(urlTest).get(new ResourceCallback<Object>() {
 			@Override
 			public void error(Throwable e) {
+				e.printStackTrace();
 			}
 
 			@Override
 			public void success(Object data) {
 				System.out.println(data);
+				System.out.println("Success!");
+				System.out.println("System properties:");
+				for (Entry<Object, Object> e : System.getProperties()
+						.entrySet()) {
+					System.out.println(e.getKey() + ":" + e.getValue());
+				}
 			}
 		}, String.class, false);
 	}
