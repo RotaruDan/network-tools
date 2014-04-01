97daa990712fd8e19ae9a23b63165e870ab4873f
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2014-04-01 15:26:48 +0200

    Adding a more verbose ouput for proxy debugging

--- network-gui/src/main/java/es/eucm/network/gui/NetworkGUI.java
+++ network-gui/src/main/java/es/eucm/network/gui/NetworkGUI.java
@@ -1,17 +1,30 @@
 package es.eucm.network.gui;
 
+import java.text.MessageFormat;
+import java.util.Map.Entry;
+
+import com.btr.proxy.util.Logger;
+import com.btr.proxy.util.Logger.LogBackEnd;
+import com.btr.proxy.util.Logger.LogLevel;
+
 import es.eucm.network.JavaRequestHelper;
 import es.eucm.network.requests.RequestHelper;
 import es.eucm.network.requests.ResourceCallback;
-
-import java.util.Map.Entry;
 
 public class NetworkGUI {
 
 	public static void main(String args[]) {
 
-		String urlTest = "http://downloads.sourceforge.net/project/e-adventure/ead2-stable/update.json";
+		Logger.setBackend(new ConsoleLogBackend());
+		String urlTest = "http://downloads.sourceforge.net/project/e-adventure/stable/update.json";
 		RequestHelper requestHelper = new JavaRequestHelper();
+
+		System.out.println("JAVA ENVIRONMENT SETTINGS");
+		for (Entry<Object, Object> e : System.getProperties()
+				.entrySet()) {
+			System.out.println(e.getKey() + ":" + e.getValue());
+		}
+
 		requestHelper.url(urlTest).get(new ResourceCallback<Object>() {
 			@Override
 			public void error(Throwable e) {
@@ -23,11 +36,24 @@ public class NetworkGUI {
 				System.out.println(data);
 				System.out.println("Success!");
 				System.out.println("System properties:");
-				for (Entry<Object, Object> e : System.getProperties()
-						.entrySet()) {
-					System.out.println(e.getKey() + ":" + e.getValue());
-				}
 			}
 		}, String.class, false);
+	}
+	
+	private static class ConsoleLogBackend implements LogBackEnd {
+
+		@Override
+		public void log(Class<?> clazz, LogLevel loglevel, String msg,
+				Object... params) {
+			System.out.print("["+loglevel.name()+"] ");
+			System.out.print(clazz.getCanonicalName() + ": ");
+			System.out.println(MessageFormat.format(msg, params));
+		}
+
+		@Override
+		public boolean isLogginEnabled(LogLevel logLevel) {
+			return true;
+		}
+
 	}
 }
